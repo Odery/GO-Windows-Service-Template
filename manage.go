c813a125a9e422914bc18bd2ecc356a63e90d2b8
Odery <kammurimini@gmail.com>
2023-06-26 18:33:27 +0300

    Added manage.go file, can start the service and control it

--- manage.go
+++ manage.go
@@ -2,4 +2,89 @@
 
 package main
 
-func startService
+import (
+	"fmt"
+	"golang.org/x/sys/windows/svc"
+	"golang.org/x/sys/windows/svc/mgr"
+	"log"
+	"time"
+)
+
+func startService(name string) error {
+	// Connect to the service manager
+	mngr, err := mgr.Connect()
+	if err != nil {
+		return err
+	}
+	defer func(mngr *mgr.Mgr) {
+		err := mngr.Disconnect()
+		if err != nil {
+			log.Println("error disconnecting from the service manager")
+		}
+	}(mngr)
+
+	// Open the service
+	s, err := mngr.OpenService(name)
+	if err != nil {
+		return fmt.Errorf("could not access the service: %v", err)
+	}
+	defer func(s *mgr.Service) {
+		err := s.Close()
+		if err != nil {
+			log.Println("error disconnecting from the service")
+		}
+	}(s)
+
+	// Start the service
+	err = s.Start("is", "manual-started")
+	if err != nil {
+		return fmt.Errorf("could not start the service: %v", err)
+	}
+	return nil
+}
+
+func controlService(name string, command svc.Cmd, to svc.State) error {
+	// Connect to the service manager
+	mngr, err := mgr.Connect()
+	if err != nil {
+		return err
+	}
+	defer func(mngr *mgr.Mgr) {
+		err := mngr.Disconnect()
+		if err != nil {
+			log.Println("error disconnecting from the service manager")
+		}
+	}(mngr)
+
+	// Open the service
+	s, err := mngr.OpenService(name)
+	if err != nil {
+		return fmt.Errorf("could not access the service: %v", err)
+	}
+	defer func(s *mgr.Service) {
+		err := s.Close()
+		if err != nil {
+			log.Println("error disconnecting from the service")
+		}
+	}(s)
+
+	// Control the service
+	status, err := s.Control(command)
+	if err != nil {
+		return fmt.Errorf("could not send control command = '%d', error: %v", command, err)
+	}
+
+	// Polling service for changes
+	timeout := time.Now().Add(10 * time.Second)
+	for status.State != to {
+		if timeout.Before(time.Now()) {
+			return fmt.Errorf("timeout for the service to change state on command: %d", command)
+		}
+		time.Sleep(300 * time.Millisecond)
+		status, err = s.Query()
+		if err != nil {
+			return fmt.Errorf("could not retireve service status after command, error: %v", err)
+		}
+	}
+	return nil
+}
